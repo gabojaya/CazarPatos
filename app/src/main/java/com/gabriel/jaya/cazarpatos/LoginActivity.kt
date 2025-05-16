cc89bae7b2b7b54e207a8f74fea8d6d758760c36
P3E009-E <P3E009-E@epn.edu.ec>
2025-05-16 10:17:28 -0500

    Add LoginActivity.kt

---
+++ app/src/main/java/com/gabriel/jaya/cazarpatos/LoginActivity.kt
@@ -0,0 +1,32 @@
+package com.gabriel.jaya.cazarpatos
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onBtnLoginClick(view: View){
+        Toast.makeText(this,"Iniciaste Sesión", Toast.LENGTH_LONG).show()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+    }
+}

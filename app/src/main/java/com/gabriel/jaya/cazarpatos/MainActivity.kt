1603fc93b901fefafc576fd2f91892bdf9109eb5
Gabriel Jaya <gabriel.jaya@epn.edu.ec>
2025-07-25 11:04:01 -0500

    Anuncios

--- app/src/main/java/com/gabriel/jaya/cazarpatos/MainActivity.kt
+++ app/src/main/java/com/gabriel/jaya/cazarpatos/MainActivity.kt
@@ -17,8 +17,17 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.util.Locale
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -27,6 +36,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textViewTime: TextView
     private lateinit var imageViewDuck: ImageView
     private lateinit var soundPool: SoundPool
+
     // Manejador para retrasar la restauración de la imagen original
     private val handler = Handler(Looper.getMainLooper())
     private var counter = 0
@@ -35,6 +45,10 @@ class MainActivity : AppCompatActivity() {
     private var soundId: Int = 0
     private var isLoaded = false
     private var gameOver = false
+
+    lateinit var mAdView: AdView
+    private var interstitialAd: InterstitialAd? = null
+    private var TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,10 +64,20 @@ class MainActivity : AppCompatActivity() {
         textViewCounter = findViewById(R.id.textViewCounter)
         textViewTime = findViewById(R.id.textViewTime)
         imageViewDuck = findViewById(R.id.imageViewDuck)
+        MobileAds.initialize(this) {}
+
+//Ads Inicialización y carga
+        mAdView = findViewById(R.id.adView)
+//mAdView.setAdSize(AdSize.BANNER)
+//mAdView.adUnitId = "ca-app-pub-3940256099942544/9214589741" //ID para testing
+//mAdView.adUnitId = "ca-app-pub-9624227872779318/1299622037" //TODO: Colocar esto antes de publicar en playStore
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+        loadInterstitialAd()
 
         //Obtener el usuario de pantalla login
         val extras = intent.extras ?: return
-        var usuario = extras.getString(EXTRA_LOGIN) ?:"Unknown"
+        var usuario = extras.getString(EXTRA_LOGIN) ?: "Unknown"
         usuario = usuario.substringBefore("@")
         textViewUser.setText(usuario)
         //Determina el ancho y largo de pantalla
@@ -94,19 +118,88 @@ class MainActivity : AppCompatActivity() {
             moveDuckRandomly()
         }
     }
+
+    private fun showInterstitial() {
+        // Show the ad if it"s ready. Otherwise toast and reload the ad.
+        if (interstitialAd != null) {
+            interstitialAd!!.show(this)
+        } else {
+            loadInterstitialAd()
+            //Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun loadInterstitialAd() {
+        val adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(this, "ca-app-pub-3940256099942544/1033173712", adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    // The interstitialAd reference will be null until
+                    // an ad is loaded.
+                    interstitialAd = ad
+                    Toast.makeText(this@MainActivity, "onAdLoaded()", Toast.LENGTH_SHORT)
+                        .show()
+                    ad.setFullScreenContentCallback(
+                        object : FullScreenContentCallback() {
+                            override fun onAdDismissedFullScreenContent() {
+                                // Called when fullscreen content is dismissed.
+                                // Make sure to set your reference to null so you don't
+                                // show it a second time.
+                                interstitialAd = null
+                                Log.d(TAG, "The ad was dismissed.")
+                            }
+
+                            override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                                // Called when fullscreen content failed to show.
+                                // Make sure to set your reference to null so you don't
+                                // show it a second time.
+                                interstitialAd = null
+                                Log.d(TAG, "The ad failed to show.")
+                            }
+
+                            override fun onAdShowedFullScreenContent() {
+                                // Called when fullscreen content is shown.
+                                Log.d(TAG, "The ad was shown.")
+                            }
+                        })
+                }
+
+                override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                    // Handle the error
+                    Log.i(TAG, loadAdError.message)
+                    interstitialAd = null
+                    val error: String = String.format(
+                        Locale.ENGLISH,
+                        "domain: %s, code: %d, message: %s",
+                        loadAdError.domain,
+                        loadAdError.code,
+                        loadAdError.message
+                    )
+                    Toast.makeText(
+                        this@MainActivity,
+                        "onAdFailedToLoad() with error: $error", Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            })
+    }
+
+
+
     private fun initializeScreen() {
         // 1. Obtenemos el tamaño de la pantalla del dispositivo
         val display = this.resources.displayMetrics
         screenWidth = display.widthPixels
         screenHeight = display.heightPixels
     }
+
     private fun moveDuckRandomly() {
         val min = imageViewDuck.getWidth()/2
         val maximoX = screenWidth - imageViewDuck.getWidth()
-        val maximoY = screenHeight - imageViewDuck.getHeight()
+        val maximoY = screenHeight - imageViewDuck.getHeight() - mAdView.height //Por AdMob
         // Generamos 2 números aleatorios, para la coordenadas x , y
         val randomX = Random.nextInt(0,maximoX - min + 1)
-        val randomY = Random.nextInt(96,maximoY - min + 1)
+        val randomY = Random.nextInt(96+min,maximoY - min + 1)
 
         imageViewDuck.animate()
             .x(randomX.toFloat())
@@ -114,11 +207,13 @@ class MainActivity : AppCompatActivity() {
             .setDuration(300) // animación suave
             .start()
     }
+
     private var countDownTimer = object : CountDownTimer(10000, 1000) {
         override fun onTick(millisUntilFinished: Long) {
             val secondsRemaining = millisUntilFinished / 1000
             textViewTime.setText("${secondsRemaining}s")
         }
+
         override fun onFinish() {
             textViewTime.setText("0s")
             gameOver = true
@@ -128,10 +223,13 @@ class MainActivity : AppCompatActivity() {
             procesarPuntajePatosCazados(nombreJugador, patosCazados.toInt()) //Firestore
         }
     }
+
     private fun initializeCountdown() {
         countDownTimer.start()
     }
+
     private fun showGameOverDialog() {
+        showInterstitial()
         val builder = AlertDialog.Builder(this)
         builder
             .setMessage(getString(R.string.dialog_message_congratulations, counter))
@@ -146,7 +244,8 @@ class MainActivity : AppCompatActivity() {
             .setCancelable(false)  // Prevents closing on outside click
         builder.create().show()
     }
-    private fun restartGame(){
+
+    private fun restartGame() {
         counter = 0
         gameOver = false
         countDownTimer.cancel()
@@ -155,41 +254,47 @@ class MainActivity : AppCompatActivity() {
         initializeCountdown()
     }
 
-    fun jugarOnline(){
+    fun jugarOnline() {
         var intentWeb = Intent()
         intentWeb.action = Intent.ACTION_VIEW
         intentWeb.data = Uri.parse("https://duckhuntjs.com/")
         startActivity(intentWeb)
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main,menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_nuevo_juego -> {
                 restartGame()
                 true
             }
+
             R.id.action_jugar_online -> {
                 jugarOnline()
                 true
             }
+
             R.id.action_ranking -> {
                 val intent = Intent(this, RankingActivity::class.java)
                 startActivity(intent)
                 true
             }
+
             R.id.action_salir -> {
                 finish()
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    fun procesarPuntajePatosCazados(nombreJugador:String, patosCazados:Int){
-        val jugador = Player(nombreJugador,patosCazados)
+    fun procesarPuntajePatosCazados(nombreJugador: String, patosCazados: Int) {
+        val jugador = Player(nombreJugador, patosCazados)
         //Trata de obtener id del documento del ranking específico,
         // si lo obtiene lo actualiza, caso contrario lo crea
         val db = Firebase.firestore
@@ -197,22 +302,19 @@ class MainActivity : AppCompatActivity() {
             .whereEqualTo("username", jugador.username)
             .get()
             .addOnSuccessListener { documents ->
-                if(documents!= null &&
+                if (documents != null &&
                     documents.documents != null &&
-                    documents.documents.count()>0
-                ){
+                    documents.documents.count() > 0
+                ) {
                     val idDocumento = documents.documents.get(0).id
                     val jugadorLeido = documents.documents.get(0).toObject(Player::class.java)
-                    if(jugadorLeido!!.huntedDucks < patosCazados )
-                    {
+                    if (jugadorLeido!!.huntedDucks < patosCazados) {
                         Log.w(EXTRA_LOGIN, "Puntaje actual mayor, por lo tanto actualizado")
                         actualizarPuntajeJugador(idDocumento, jugador)
-                    }
-                    else{
+                    } else {
                         Log.w(EXTRA_LOGIN, "No se actualizo puntaje, por ser menor al actual")
                     }
-                }
-                else{
+                } else {
                     ingresarPuntajeJugador(jugador)
                 }
             }
@@ -221,30 +323,37 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Error al obtener datos de jugador", Toast.LENGTH_LONG).show()
             }
     }
-    fun ingresarPuntajeJugador(jugador:Player){
+
+    fun ingresarPuntajeJugador(jugador: Player) {
         val db = Firebase.firestore
         db.collection("ranking")
             .add(jugador)
             .addOnSuccessListener { documentReference ->
-                Toast.makeText(this,"Puntaje usuario ingresado exitosamente", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Puntaje usuario ingresado exitosamente", Toast.LENGTH_LONG)
+                    .show()
             }
             .addOnFailureListener { exception ->
                 Log.w(EXTRA_LOGIN, "Error adding document", exception)
-                Toast.makeText(this,"Error al ingresar el puntaje", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Error al ingresar el puntaje", Toast.LENGTH_LONG).show()
             }
     }
-    fun actualizarPuntajeJugador(idDocumento:String, jugador:Player){
+
+    fun actualizarPuntajeJugador(idDocumento: String, jugador: Player) {
         val db = Firebase.firestore
         db.collection("ranking")
             .document(idDocumento)
             //.update(contactoHashMap)
             .set(jugador) //otra forma de actualizar
             .addOnSuccessListener {
-                Toast.makeText(this,"Puntaje de usuario actualizado exitosamente", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this,
+                    "Puntaje de usuario actualizado exitosamente",
+                    Toast.LENGTH_LONG
+                ).show()
             }
             .addOnFailureListener { exception ->
                 Log.w(EXTRA_LOGIN, "Error updating document", exception)
-                Toast.makeText(this,"Error al actualizar el puntaje" , Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Error al actualizar el puntaje", Toast.LENGTH_LONG).show()
             }
     }
 
@@ -261,4 +370,6 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         soundPool.release()
     }
+
+
 }
